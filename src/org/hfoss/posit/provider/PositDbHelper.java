f68ed83131185932bdf94bb8cb30a0f59d64df6f
Prasanna Gautam <prasannagautam@gmail.com>
2010-06-29 15:41:08 -0400

    Applied Greg's patch to show the truncated UUID on the ListFindsActivity and FindActivity

--- src/org/hfoss/posit/provider/PositDbHelper.java
+++ src/org/hfoss/posit/provider/PositDbHelper.java
@@ -110,7 +110,7 @@ public class PositDbHelper extends SQLiteOpenHelper {
 	//	 in an attempt to deal with the length of the new UUIDs
 	public static final String[] list_row_data = { 
 		FINDS_ID,
-//		FINDS_GUID,  
+		FINDS_GUID,  
 		FINDS_NAME,
 		FINDS_DESCRIPTION,
 		FINDS_LATITUDE,
@@ -122,7 +122,7 @@ public class PositDbHelper extends SQLiteOpenHelper {
 
 	public static final int[] list_row_views = {
 		R.id.row_id,		    
-//		R.id.barcode_id,
+		R.id.idNumberText,
 		R.id.name_id, 
 		R.id.description_id,
 		R.id.latitude_id,
