e795ae914b9d4a57b4dd39ef9ca6544c804563af
Prasanna Gautam <prasannagautam@gmail.com>
2010-06-30 11:01:59 -0400

    changed the package name to org.hfoss.posit.android because of problems in uploading application to marketplace.

---
+++ src/org/hfoss/posit/android/Constants.java
@@ -0,0 +1,25 @@
+package org.hfoss.posit.android;
+
+public class Constants {
+	
+	public static final int ERR_PASSWORD_MISSING = 0x13;
+	public static final int ERR_IMEI_MISSING = 0x14;
+	public static final int ERR_EMAIL_INVALID = 0x12;
+	public static final int ERR_PASSWORD_UNMATCHED = 0x11;
+	public static final int ERR_PASSWORD2_INVALID = 0x10;
+	public static final int ERR_PASSWORD1_INVALID = 0x0F;
+	public static final int ERR_LASTNAME_MISSING = 0x0E;
+	public static final int ERR_FIRSTNAME_MISSING = 0x0D;
+	public static final int ERR_EMAIL_MISSING = 0x0C;
+	public static final int ERR_AUTHKEY_MISSING = 0x0B;
+	public static final int ERR_AUTHKEY_INVALID = 0x0A;
+	public static final int REGISTRATION_EMAILEXISTS = 0x09;
+	public static final int AUTHN_NOTLOGGEDIN = 0x08;
+	public static final int AUTHN_FAILED = 0x07;
+	public static final int AUTHN_OK = 0x06;
+	public static final int IMAGES_GET_THUMBNAILS = 0x04;
+	public static final int IMAGES_GET_FULL = 0x03;
+	public static final int PROJECTS_CLOSED = 0x02;
+	public static final int PROJECTS_OPEN = 0x01;
+	public static final int PROJECTS_ALL = 0x00;
+}
