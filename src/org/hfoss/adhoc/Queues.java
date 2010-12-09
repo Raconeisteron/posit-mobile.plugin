1ec1a86145d1a08ebaf9607a831c2e5301c8fde9
Ralph Morelli <ram8647@gmail.com>
2010-12-09 16:04:38 -0500

    Removed Adhoc, QueueService, Queues classes.  These are removed from my local repository but have not yet been removed from the main repository from which this clone was produced.

--- src/org/hfoss/adhoc/Queues.java
+++
@@ -1,9 +0,0 @@
-package org.hfoss.adhoc;
-
-import java.util.concurrent.ArrayBlockingQueue;
-
-public class Queues {
-	
-	public static ArrayBlockingQueue<AdhocData> outputQueue = new ArrayBlockingQueue<AdhocData>(50);
-	public static ArrayBlockingQueue<AdhocData> inputQueue  = new ArrayBlockingQueue<AdhocData>(50);
-}
