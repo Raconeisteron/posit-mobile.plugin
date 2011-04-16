804a36d68d6bdf63c62be543ffd2258bb7f4a8c8
stanislav 6 <stanislav.6@gmail.com>
2011-04-15 18:33:29 -0700

    added package name attribute to <Plugin ... /> element for lessy typing

--- src/org/hfoss/posit/android/api/FindPluginManager.java
+++ src/org/hfoss/posit/android/api/FindPluginManager.java
@@ -56,20 +56,21 @@ public class FindPluginManager {
 			NodeList plugin_nodes = (NodeList)xpath.evaluate("PluginsPreferences/FindPlugins/Plugin", document, XPathConstants.NODESET);
 			for(int ii = 0; ii < plugin_nodes.getLength(); ++ii){
 				if(plugin_nodes.item(ii).getAttributes().getNamedItem("active").getTextContent().compareTo("true") == 0){
+					String package_name = plugin_nodes.item(ii).getAttributes().getNamedItem("package").getTextContent();
 					String find_factory_name = plugin_nodes.item(ii).getAttributes().getNamedItem("find_factory").getTextContent();
 					String find_data_manager_name = plugin_nodes.item(ii).getAttributes().getNamedItem("find_data_manager").getTextContent();
 					String findactivity_name = plugin_nodes.item(ii).getAttributes().getNamedItem("findactivity_class").getTextContent();
 					String listfindsactivity_name = plugin_nodes.item(ii).getAttributes().getNamedItem("listfindsactivity_class").getTextContent();
 					
 					@SuppressWarnings({ "rawtypes" })
-					Class new_class = Class.forName(find_factory_name);
+					Class new_class = Class.forName(package_name + "." + find_factory_name);
 					mFindFactory = (FindFactory)new_class.getMethod("getInstance", null).invoke(null, null);
 					
-					new_class = Class.forName(find_data_manager_name);
+					new_class = Class.forName(package_name + "." + find_data_manager_name);
 					mFindDataManager = (FindDataManager)new_class.getMethod("getInstance", null).invoke(null, null);
 
-					mFindActivityClass = (Class<FindActivity>)Class.forName(findactivity_name);
-					mListFindsActivityClass = (Class<ListFindsActivity>)Class.forName(listfindsactivity_name);
+					mFindActivityClass = (Class<FindActivity>)Class.forName(package_name + "." + findactivity_name);
+					mListFindsActivityClass = (Class<ListFindsActivity>)Class.forName(package_name + "." + listfindsactivity_name);
 					
 					break;
 				}
