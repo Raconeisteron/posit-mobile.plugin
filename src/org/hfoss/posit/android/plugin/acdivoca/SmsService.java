e0f4d10a27218537de431cc5927f3757b3317a97
Ralph Morelli <ram8647@gmail.com>
2011-06-26 10:48:50 -0400

    Forgot to add the service class to last commit -- big omission.

---
+++ src/org/hfoss/posit/android/plugin/acdivoca/SmsService.java
@@ -0,0 +1,212 @@
+/*
+ * File: SmsService.java
+ * 
+ * Copyright (C) 2011 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of the ACDI/VOCA plugin for POSIT, Portable Open Search 
+ * and Identification Tool.
+ *
+ * This plugin is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
+ * 
+ */
+
+package org.hfoss.posit.android.plugin.acdivoca;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.telephony.SmsManager;
+import android.util.Log;
+
+public class SmsService extends Service {
+	public static final String TAG = "AcdiVocaSmsManager";
+	
+	private  BroadcastReceiver mReceiver;
+	private int nMsgsSent = 0;
+	private int nMsgsPending = 0;
+	private int mBroadcastsOutstanding = 0;
+	private String mErrorMsg = ""; // Set to last error by BroadcastReceiver, not currently used
+
+	private SendMessagesTask sendMessagesTask;
+
+	private ArrayList<String> mMessages;
+	private String mPhoneNumber;
+	
+	/**
+	 * This service is not accepting bindings.
+	 */
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+	}
+
+	/**
+	 * Starts the service.  Creates a AsyncTask to perform the actual
+	 * sending of messages in a background task.
+	 */
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		mMessages = intent.getStringArrayListExtra("messages");
+		mPhoneNumber = intent.getStringExtra("phonenumber");
+		Log.i(TAG, "Started background service, phone = " + mPhoneNumber +
+				" nMessages = " + mMessages.size());
+
+		sendMessagesTask = new SendMessagesTask();
+		sendMessagesTask.execute(this);
+		
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	/**
+	 * A broadcast receiver receives an intent about a sent message and
+	 * reports whether it was sent successfully or failed. 
+	 * @param receiver that was created for an individual message
+	 * @param resultCode whether the message succeeded in being sent or not
+	 * @param intent the msgid is the ACTION of the intent
+	 */
+	private synchronized void handleSentMessage (BroadcastReceiver receiver, 
+			int resultCode, Intent intent, String smsMsg)  {
+		String msgId = intent.getAction();  //   arg1.getStringExtra("msgid")
+		AcdiVocaMessage avMsg = new AcdiVocaMessage(smsMsg);
+		AcdiVocaDbHelper db =  new AcdiVocaDbHelper(this);
+		switch (resultCode)  {
+		case Activity.RESULT_OK:
+			Log.d (TAG, "Received OK, msgid = " + msgId);
+			db.updateMessageStatus(avMsg, AcdiVocaDbHelper.MESSAGE_STATUS_SENT);
+			++nMsgsSent;
+			break;
+		case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+			Log.e(TAG, "Received  generic failure, msgid =  " + msgId);
+			db.updateMessageStatus(avMsg, AcdiVocaDbHelper.MESSAGE_STATUS_PENDING);
+			++nMsgsPending;
+			mErrorMsg = "Generic Failure";
+			break;
+		case SmsManager.RESULT_ERROR_NO_SERVICE:
+			Log.e(TAG, "Received  No service, msgid =  " + msgId);
+			db.updateMessageStatus(avMsg, AcdiVocaDbHelper.MESSAGE_STATUS_PENDING);
+			++nMsgsPending;
+			mErrorMsg = "No cellular service";
+			break;
+		case SmsManager.RESULT_ERROR_NULL_PDU:
+			Log.e(TAG, "Received Null PDU, msgid =  " + msgId);
+			db.updateMessageStatus(avMsg, AcdiVocaDbHelper.MESSAGE_STATUS_PENDING);
+			++nMsgsPending;
+			mErrorMsg = "Null PDU error";
+			break;
+		case SmsManager.RESULT_ERROR_RADIO_OFF:
+			Log.e(TAG, "Received  Radio off, msgid =  " + msgId);
+			db.updateMessageStatus(avMsg, AcdiVocaDbHelper.MESSAGE_STATUS_PENDING);
+			++nMsgsPending;
+			mErrorMsg = "Texting is off";
+			break;
+		}
+	}
+	
+	class SendMessagesTask extends AsyncTask<Context, Integer, String> {
+		public static final String TAG = "AsyncTask";
+		
+		private Context context;
+		
+		@Override
+		protected String doInBackground(Context... contexts) {
+			Log.i(TAG, "doInBackground");
+			this.context = contexts[0];
+			transmitMessages(context, mMessages);
+			return null;
+		}
+
+		
+		protected void transmitMessages(Context context, ArrayList<String> messages) {
+			Iterator<String> it = messages.iterator();
+			mBroadcastsOutstanding = messages.size();
+			while (it.hasNext()) {
+				final String message = it.next();
+				String[] msgparts = message.split(AttributeManager.PAIRS_SEPARATOR);
+				String[] firstPair = msgparts[0].split(AttributeManager.ATTR_VAL_SEPARATOR);
+				String msgid = firstPair[1];
+				
+				Intent sendIntent = new Intent(msgid);
+				IntentFilter intentFilter = new IntentFilter(msgid);
+				PendingIntent sentIntent = PendingIntent.getBroadcast(context, 0, sendIntent, 0);
+
+				// Not really used -- we are not processing DELIVERED broadcasts.
+				Intent delivered = new Intent ("SMS DELIVERED");
+				PendingIntent deliveryIntent = PendingIntent.getBroadcast(context, 0,delivered, 0);
+
+				// This receiver will be sent a result from the radio device as to whether the message was sent
+				mReceiver = new BroadcastReceiver() {
+					@Override
+					public synchronized void onReceive(Context arg0, Intent arg1) {
+						handleSentMessage(this, getResultCode(), arg1, message);
+						--mBroadcastsOutstanding;
+						unregisterReceiver(this);
+						Log.i(TAG, "Broadcasts outstanding  = " + mBroadcastsOutstanding);
+					}
+				};
+				context.registerReceiver(mReceiver, intentFilter);
+				
+				try {
+				SmsManager sms = SmsManager.getDefault();
+				sms.sendTextMessage(mPhoneNumber, null, message, sentIntent, deliveryIntent);    
+				Log.i(TAG,"SMS Sent: " + msgid + " to " + mPhoneNumber);
+				} catch (IllegalArgumentException e) {
+					Log.e(TAG, "IllegalArgumentException, probably phone nubmer = " + mPhoneNumber);
+					mErrorMsg = e.getMessage();
+					e.printStackTrace();
+					return;
+				}
+			}
+		}
+		
+		
+		@Override
+		protected void onCancelled() {
+			Log.i(TAG, "onCancelled");
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Log.i(TAG, "onPostExecute, broadcasts outstanding = " + mBroadcastsOutstanding);
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			Log.i(TAG, "onPreExecute");
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			Log.i(TAG, "onProgressUpdate");
+			super.onProgressUpdate(values);
+		}
+	}
+}
