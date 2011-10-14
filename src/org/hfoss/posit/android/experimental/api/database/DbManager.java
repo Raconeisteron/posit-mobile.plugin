3632bafc832e6d17384df163ef1cd3a6a48bebd5
Rachel Foecking <captainfoecking@gmail.com>
2011-10-14 14:24:14 -0400

    Added in some of the syncing logic from old posit.  Two new DB tables: find_history to track changes to finds and sync_history to track past syncs.  Sends changed finds to the server depending on whether they've been changed since the last sync.  Doesn't get new finds from the server yet.

--- src/org/hfoss/posit/android/experimental/api/database/DbManager.java
+++ src/org/hfoss/posit/android/experimental/api/database/DbManager.java
@@ -25,9 +25,16 @@ package org.hfoss.posit.android.experimental.api.database;
 
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import org.hfoss.posit.android.experimental.api.Find;
+import org.hfoss.posit.android.experimental.api.FindHistory;
+import org.hfoss.posit.android.experimental.api.SyncHistory;
 import org.hfoss.posit.android.experimental.api.User;
 import org.hfoss.posit.android.experimental.plugin.FindPluginManager;
 import org.hfoss.posit.android.experimental.plugin.acdivoca.AcdiVocaFind;
@@ -36,6 +43,7 @@ import org.hfoss.posit.android.experimental.plugin.acdivoca.AcdiVocaUser;
 
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.GenericRawResults;
 import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.Where;
@@ -44,6 +52,7 @@ import com.j256.ormlite.table.TableUtils;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.format.DateFormat;
 import android.util.Log;
 
 /**
@@ -59,8 +68,7 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 
 	public static final int DELETE_FIND = 1;
 	public static final int UNDELETE_FIND = 0;
-	public static final String WHERE_NOT_DELETED = " " + AcdiVocaFind.DELETED
-			+ " != " + DELETE_FIND + " ";
+	public static final String WHERE_NOT_DELETED = " " + AcdiVocaFind.DELETED + " != " + DELETE_FIND + " ";
 	public static final String DATETIME_NOW = "`datetime('now')`";
 
 	public static final String FINDS_HISTORY_TABLE = "acdi_voca_finds_history";
@@ -69,7 +77,8 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 	// DAO objects used to access the Db tables
 	private Dao<User, Integer> userDao = null;
 	private Dao<Find, Integer> findDao = null;
-
+	private Dao<FindHistory, Integer> findHistoryDao = null;
+	private Dao<SyncHistory, Integer> syncHistoryDao = null;
 
 	/**
 	 * Constructor just saves and opens the Db.
@@ -91,20 +100,23 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 			findClass.getMethod("createTable", ConnectionSource.class).invoke(null, connectionSource);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 		
+		}
 		User.createTable(connectionSource, getUserDao());
+		FindHistory.createTable(connectionSource);
+		SyncHistory.createTable(connectionSource);
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource,
-			int oldVersion, int newVersion) {
+	public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
 		try {
 			Log.i(TAG, "onUpgrade");
 			TableUtils.dropTable(connectionSource, User.class, true);
-			
+			TableUtils.dropTable(connectionSource, FindHistory.class, true);
+			TableUtils.dropTable(connectionSource, SyncHistory.class, true);
+
 			Class<Find> findClass = FindPluginManager.getInstance().getFindClass();
 			TableUtils.dropTable(connectionSource, findClass, true);
-			
+
 			// after we drop the old databases, we create the new ones
 			onCreate(db, connectionSource);
 		} catch (SQLException e) {
@@ -115,6 +127,7 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 
 	/**
 	 * Fetches all finds currently in the database.
+	 * 
 	 * @return A list of all the finds.
 	 */
 	public List<? extends Find> getAllFinds() {
@@ -128,15 +141,15 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 		return list;
 
 	}
-	
-	public List<Find> getFindsByProjectId(int projectId){
+
+	public List<Find> getFindsByProjectId(int projectId) {
 		List<Find> list = null;
 		try {
 			QueryBuilder<Find, Integer> builder = getFindDao().queryBuilder();
 			Where<Find, Integer> where = builder.where();
 			where.eq(Find.PROJECT_ID, projectId);
 			PreparedQuery<Find> preparedQuery = builder.prepare();
-			
+
 			list = getFindDao().query(preparedQuery);
 		} catch (SQLException e) {
 			Log.e(TAG, "Database error getting finds: " + e.getMessage());
@@ -146,7 +159,9 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 
 	/**
 	 * Looks up a find by its ID.
-	 * @param id the id of the find to look up
+	 * 
+	 * @param id
+	 *            the id of the find to look up
 	 * @return the find
 	 */
 	public Find getFindById(int id) {
@@ -158,15 +173,52 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 		}
 		return find;
 	}
-	
-	public int updateStatus(Find find, int status) {
+
+	/**
+	 * Inserts this find into the database.
+	 * 
+	 * @param dao
+	 *            the DAO object provided by the ORMLite helper class.
+	 * @return the number of rows inserted.
+	 */
+
+	public int insert(Find find) {
 		int rows = 0;
 		try {
-			find.setStatus(status);
-			rows = getFindDao().update(find);
-			if (rows == 1)
-				Log.i(TAG, "Updated find status:  " + this.toString());
+			find.setAction(FindHistory.ACTION_CREATE);
+			rows = getFindDao().create(find);
+			if (rows == 1) {
+				Log.i(TAG, "Inserted find:  " + this.toString());
+				recordChangedFind(new FindHistory(find, FindHistory.ACTION_CREATE));
+			}
 			else {
+				Log.e(TAG, "Db Error inserting find: " + this.toString());
+				rows = 0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rows;
+	}
+
+	/**
+	 * Updates this find in the database with the given values.
+	 * 
+	 * @param dao
+	 *            the DAO provided by the ORMLite helper class.
+	 * @param values
+	 *            a ContentValues object containing all of the values to update.
+	 * @return the number of rows updated.
+	 */
+	public int update(Find find) {
+		int rows = 0;
+		try {
+			find.setAction(FindHistory.ACTION_UPDATE);
+			rows = getFindDao().update(find);
+			if (rows == 1) {
+				Log.i(TAG, "Updated find:  " + this.toString());
+				recordChangedFind(new FindHistory(find, FindHistory.ACTION_UPDATE));
+			} else {
 				Log.e(TAG, "Db Error updating find: " + this.toString());
 				rows = 0;
 			}
@@ -176,20 +228,179 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 		return rows;
 	}
 
+	/**
+	 * Deletes this find.
+	 * 
+	 * @param dao
+	 *            the DAO provided by the ORMLite helper class.
+	 * @return the number of rows deleted.
+	 */
+	public int delete(Find find) {
+		int rows = 0;
+		try {
+			rows = getFindDao().delete(find);
+			if (rows == 1)
+				Log.i(TAG, "Deleted find:  " + this.toString());
+			else {
+				Log.e(TAG, "Db Error deleting find: " + this.toString());
+				rows = 0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rows;
+
+	}
+
+	public int updateStatus(Find find, int status) {
+		int rows = 0;
+		try {
+			find.setStatus(status);
+			rows = getFindDao().update(find);
+			if (rows == 1) {
+				Log.i(TAG, "Updated find status:  " + this.toString());
+				//recordChangedFind(new FindHistory(find, FindHistory.ACTION_UPDATE));
+			} else {
+				Log.e(TAG, "Db Error updating find: " + this.toString());
+				rows = 0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rows;
+	}
 
 	public int updateSyncOperation(Find find, int operation) {
 		int rows = 0;
 		try {
 			find.setSyncOperation(operation);
 			rows = getFindDao().update(find);
-			if (rows == 1)
+			if (rows == 1) {
 				Log.i(TAG, "Updated find sync operation:  " + this.toString());
-			else {
+				//recordChangedFind(new FindHistory(find, FindHistory.ACTION_UPDATE));
+			} else {
 				Log.e(TAG, "Db Error updating sync operation in find: " + this.toString());
 				rows = 0;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+		return rows;
+	}
+
+	public List<Find> getChangedFinds(int projectId) {
+		List<Find> finds = null;
+		try {
+			// Find find1 = new Find();
+			// find1.setProject_id(0);
+			// find1.setGuid("gagagagaaaa");
+			// Find find2 = new Find();
+			// find2.setProject_id(0);
+			// find2.setGuid("lolwhat");
+			// FindHistory history = new FindHistory(find1,
+			// FindHistory.ACTION_CREATE);
+			// FindHistory history2 = new FindHistory(find2,
+			// FindHistory.ACTION_CREATE);
+			// getFindDao().create(find1);
+			// getFindDao().create(find2);
+			// getFindHistoryDao().create(history);
+			// getFindHistoryDao().create(history2);
+
+			Date lastSync = getTimeOfLastSync();
+
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+			String lastSyncString = formatter.format(lastSync);
+
+			GenericRawResults<String[]> raw = getFindHistoryDao().queryRaw(
+					"SELECT DISTINCT findhistory" + "." + FindHistory.FIND + ",findhistory" + "."
+							+ FindHistory.FIND_ACTION + " FROM findhistory, find" + " WHERE find." + Find.PROJECT_ID
+							+ " = " + projectId + " AND " + "findhistory" + "." + FindHistory.FIND_ACTION + "!= '"
+							+ FindHistory.ACTION_DELETE + "' AND findhistory." + FindHistory.TIME + " > '"
+							+ lastSyncString + "'");
+
+			List<String[]> results = raw.getResults();
+			finds = new ArrayList<Find>();
+			for (String[] result : results) {
+				int findId = Integer.parseInt(result[0]);
+				Find find = getFindDao().queryForId(Integer.parseInt(result[0]));
+				finds.add(find);
+			}
+			Log.i(TAG, "rawlist: " + results.toString());
+			Log.i(TAG, "FINAL FINDS: " + finds);
+
+			// Iterator<String[]> it = raw.iterator();
+			// while (it.hasNext()) {
+			// String next[] = it.next();
+			// Log.i(TAG, "raw: " + next.toString() + next[0] + next[1]);
+			// }
+			// QueryBuilder<FindHistory, Integer> builder =
+			// getFindHistoryDao().queryBuilder();
+			// Where<FindHistory, Integer> where = builder.where();
+			// where.eq("find." + Find.PROJECT_ID, projectId);
+
+			// where.and();
+			// where.not(where.eq(FindHistory.FIND_ACTION,
+			// FindHistory.ACTION_DELETE));
+			// PreparedQuery<FindHistory> query = builder.prepare();
+			// findHistories = getFindHistoryDao().query(query);
+
+		} catch (SQLException e) {
+			Log.e(TAG, "Db error getting finds changed since last sync: " + e.getMessage());
+		}
+		return finds;
+	}
+
+	/**
+	 * Gets the date of the last sync.
+	 * 
+	 * @return the date
+	 */
+	public Date getTimeOfLastSync() {
+		Date lastSync = null;
+		try {
+			QueryBuilder<SyncHistory, Integer> builder = getSyncHistoryDao().queryBuilder();
+			builder.orderBy(SyncHistory.TIME, false);
+			PreparedQuery<SyncHistory> query = builder.prepare();
+			SyncHistory syncHistory = getSyncHistoryDao().queryForFirst(query);
+			if (syncHistory == null) // Never synced before
+				lastSync = new Date(0); // Jan. 1st 1970 lol
+			else
+				lastSync = syncHistory.getTime();
+		} catch (SQLException e) {
+			Log.e(TAG, "Db error getting time of last sync: " + e.getMessage());
+		}
+		return lastSync;
+	}
+
+	/**
+	 * Used to track syncs whenever they occur.
+	 * 
+	 * @param findHistory
+	 * @return the number of rows changed if successful
+	 */
+	 public int recordSync(SyncHistory syncHistory) {
+		int rows = 0;
+		try {
+			rows = getSyncHistoryDao().create(syncHistory);
+		} catch (SQLException e) {
+			Log.e(TAG, "Db error recording a sync in sync_history: " + e.getMessage());
+		}
+		return rows;
+	}
+
+	/**
+	 * Used to record a change in the find history table whenever a find is
+	 * updated, deleted, or created.
+	 * 
+	 * @param findHistory
+	 * @return the number of rows changed if successful
+	 */
+	private int recordChangedFind(FindHistory findHistory) {
+		int rows = 0;
+		try {
+			rows = getFindHistoryDao().create(findHistory);
+		} catch (SQLException e) {
+			Log.e(TAG, "Db error inserting a find history: " + e.getMessage());
 		}
 		return rows;
 	}
@@ -211,8 +422,8 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 	}
 
 	/**
-	 * Returns the Database Access Object (DAO) for the Find class. It
-	 * will create it or just give the cached value.
+	 * Returns the Database Access Object (DAO) for the Find class. It will
+	 * create it or just give the cached value.
 	 */
 	public Dao<Find, Integer> getFindDao() {
 		if (findDao == null) {
@@ -227,7 +438,37 @@ public class DbManager extends OrmLiteSqliteOpenHelper {
 		return findDao;
 	}
 
+	/**
+	 * Returns the Database Access Object (DAO) for the FindHistory class. It
+	 * will create it or just give the cached value.
+	 */
+	public Dao<FindHistory, Integer> getFindHistoryDao() {
+		if (findHistoryDao == null) {
+			try {
+				findHistoryDao = getDao(FindHistory.class);
+			} catch (SQLException e) {
+				Log.e(TAG, "Get find DAO failed.");
+				e.printStackTrace();
+			}
+		}
+		return findHistoryDao;
+	}
 
+	/**
+	 * Returns the Database Access Object (DAO) for the AcdiVocaUser class. It
+	 * will create it or just give the cached value.
+	 */
+	public Dao<SyncHistory, Integer> getSyncHistoryDao() {
+		if (syncHistoryDao == null) {
+			try {
+				syncHistoryDao = getDao(SyncHistory.class);
+			} catch (SQLException e) {
+				Log.e(TAG, "Get user DAO failed.");
+				e.printStackTrace();
+			}
+		}
+		return syncHistoryDao;
+	}
 
 	/**
 	 * Close the database connections and clear any cached DAOs.
