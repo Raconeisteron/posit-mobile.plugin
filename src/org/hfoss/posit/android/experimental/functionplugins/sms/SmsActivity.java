b5eac553cc56e85519d4c135e5cc1721142b56a1
Ryan McLeod <exitinglyboring@gmail.com>
2011-12-10 12:42:00 -0500

    Ryan's latest changes on issue #278:
    - Added Settings for the SMS plugin, which currently are a default phone number and a default message prefix, which is currently "~_". Currently, this can be overridden for individual messages when sending a Find.
    - Added Toasts for when a message is sent or fails to send. Actually, these results weren't even reaching the BroadcastReceiver before, but that's fixed now.
    - Removed all the unused code from SmsService

--- src/org/hfoss/posit/android/experimental/functionplugins/sms/SmsActivity.java
+++ src/org/hfoss/posit/android/experimental/functionplugins/sms/SmsActivity.java
@@ -10,7 +10,9 @@ import org.hfoss.posit.android.experimental.R;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,10 +21,11 @@ import android.widget.EditText;
 public class SmsActivity extends Activity {
 
 	private static final String TAG = "SmsActivity";
-	private static final char[] RESERVED_CHARS = { '\\' , ',' }; 
+	private static final char[] RESERVED_CHARS = { '\\', ',' };
 	private static final char ESCAPE_CHAR = '\\';
 
 	private EditText mEditPhoneNum;
+	private EditText mEditPrefix;
 	private Button mSendButton;
 	private List<Entry<String, Object>> mEntries;
 
@@ -31,6 +34,7 @@ public class SmsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.send_sms);
 		mEditPhoneNum = (EditText) findViewById(R.id.phoneEditText);
+		mEditPrefix = (EditText) findViewById(R.id.messagePrefixEditText);
 		mSendButton = (Button) findViewById(R.id.sendButton);
 		mSendButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -41,11 +45,20 @@ public class SmsActivity extends Activity {
 				finish();
 			}
 		});
+		// Get default phone # and default message prefix
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		String defaultNum = preferences.getString(
+				getString(R.string.defaultPhoneNumKey), "");
+		String defaultPrefix = preferences.getString(
+				getString(R.string.messagePrefixKey),
+				getString(R.string.messagePrefixDefault));
+		mEditPhoneNum.setText(defaultNum);
+		mEditPrefix.setText(defaultPrefix);
 
 		// Retrieve DbEntries of Find from intent
 		ContentValues cv = getIntent().getParcelableExtra("DbEntries");
-		mEntries = new ArrayList<Entry<String, Object>>(
-				cv.valueSet());
+		mEntries = new ArrayList<Entry<String, Object>>(cv.valueSet());
 
 		// Next sort the list by attribute (string) value. It is important that
 		// attribute values be transmitted and interpreted in the same order.
@@ -71,11 +84,14 @@ public class SmsActivity extends Activity {
 	 */
 	private String getMessage(List<Entry<String, Object>> entries) {
 		StringBuilder builder = new StringBuilder();
+		// Add attribute values
 		for (Entry<String, Object> entry : entries) {
 			if (builder.length() > 0)
 				builder.append(",");
 			builder.append(getEncoding(entry.getValue()));
 		}
+		// Add message prefix
+		builder.insert(0, mEditPrefix.getText().toString());
 		return builder.toString();
 	}
 
@@ -98,17 +114,18 @@ public class SmsActivity extends Activity {
 		String strenc = val.toString();
 		// Escape reserved characters
 		StringBuilder builder = new StringBuilder();
-		for(int i = 0; i < strenc.length(); i++) {
+		for (int i = 0; i < strenc.length(); i++) {
 			char c = strenc.charAt(i);
 			Boolean reserved = false;
-			for(char d : RESERVED_CHARS) {
+			for (char d : RESERVED_CHARS) {
 				if (c == d) {
 					reserved = true;
 					break;
 				}
 			}
 			// The escape character itself must also be escaped
-			if (c == ESCAPE_CHAR) reserved = true;
+			if (c == ESCAPE_CHAR)
+				reserved = true;
 			if (reserved) {
 				builder.append("\\" + c);
 			} else {
