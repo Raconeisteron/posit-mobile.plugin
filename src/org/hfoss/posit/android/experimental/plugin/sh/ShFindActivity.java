e632a26034181ad02d5a31ab66105f98d761c73f
Ralph Morelli <ram8647@gmail.com>
2011-12-29 00:28:44 -0500

    Added comments and did minor cleanup to ShFind plugin.

--- src/org/hfoss/posit/android/experimental/plugin/sh/ShFindActivity.java
+++ src/org/hfoss/posit/android/experimental/plugin/sh/ShFindActivity.java
@@ -1,4 +1,22 @@
-/**
+/*
+ * File: ShFindActivity.java
+ * 
+ * Copyright (C) 2011 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of POSIT, Portable Open Source Information Tool. 
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
  * 
  */
 package org.hfoss.posit.android.experimental.plugin.sh;
@@ -6,28 +24,22 @@ package org.hfoss.posit.android.experimental.plugin.sh;
 import org.hfoss.posit.android.experimental.R;
 import org.hfoss.posit.android.experimental.api.Find;
 import org.hfoss.posit.android.experimental.api.activity.FindActivity;
-import org.hfoss.posit.android.experimental.plugin.acdivoca.AcdiVocaFind;
-import org.hfoss.posit.android.experimental.plugin.outsidein.OutsideInFind;
-
-import android.content.ContentValues;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 /**
- * FindActivity subclass for Outside In plugin.
- * 
+ * FindActivity subclass for ShFind plugin. This class
+ * presents the UI (View) for creating or editing ShFinds.
  */
 public class ShFindActivity extends FindActivity {
 
 	private static final String TAG = "ShFindActivity";
 
+	/**
+	 * Creates the UI elements that extend the Basic Find's UI.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.i(TAG, "onCreate()");
@@ -35,22 +47,14 @@ public class ShFindActivity extends FindActivity {
 		
 		((RadioButton)findViewById(R.id.pickupRadio)).setOnClickListener(this);
 		((RadioButton)findViewById(R.id.dropoffRadio)).setOnClickListener(this);
-
-	}
-
-	@Override
-	protected void initializeListeners() {
-		super.initializeListeners();
-	}
-
-	public void onClick(View v) {
-		super.onClick(v);
 	}
 	
 	/**
-	 * Retrieves values from the ShFind fields and stores them in a Find instance. 
-	 * This method is invoked from the Save menu item. It also marks the find
-	 * 'unsynced' so it will be updated to the server.
+	 * Retrieves values from the ShFind fields and stores them in 
+	 * a Find instance. Note that it also calls superclass method.
+	 * 
+	 * This method is invoked from the Save menu item. 
+	 * Finds are marked 'unsynced' after this method.
 	 * 
 	 * @return a new Find object with data from the view.
 	 */
@@ -66,10 +70,13 @@ public class ShFindActivity extends FindActivity {
 		if (rb2 != null && rb2.isChecked()){
 			find.setStopType(ShFind.DROPOFF);
 		}
-
 		return find;
 	}
 
+	/**
+	 * Adds UI elements for the ShFind to the element
+	 * displayed by the superclass method.
+	 */
 	@Override
 	protected void displayContentInView(Find find) {
 		super.displayContentInView(find);
@@ -82,5 +89,4 @@ public class ShFindActivity extends FindActivity {
 		if (val == ShFind.DROPOFF)
 			rb2.setChecked(true);		
 	}
-
 }
--- src/org/hfoss/posit/android/experimental/plugin/sh/ShListFindsActivity.java
+++ src/org/hfoss/posit/android/experimental/plugin/sh/ShListFindsActivity.java
@@ -1,23 +1,36 @@
+/*
+ * File: ShListindsActivity.java
+ * 
+ * Copyright (C) 2011 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of POSIT, Portable Open Source Information Tool. 
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
+ * 
+ */
+
 package org.hfoss.posit.android.experimental.plugin.sh;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.List;
-
-import org.hfoss.posit.android.experimental.Constants;
 import org.hfoss.posit.android.experimental.R;
 import org.hfoss.posit.android.experimental.api.Camera;
 import org.hfoss.posit.android.experimental.api.Find;
 import org.hfoss.posit.android.experimental.api.activity.ListFindsActivity;
 import org.hfoss.posit.android.experimental.plugin.FindPluginManager;
-
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,22 +38,24 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+/**
+ * Displays a List of ShFinds in a contact list View. 
+ *
+ */
 public class ShListFindsActivity extends ListFindsActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 	}
 
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 	}
 	
 	/**
-	 * Sets up a custom list adapter specific to Clp finds.
+	 * Sets up a custom list adapter specific to ShFinds. 
 	 */
 	@Override
 	protected ListAdapter setUpAdapter() {
@@ -50,7 +65,7 @@ public class ShListFindsActivity extends ListFindsActivity {
 		int resId = getResources().getIdentifier(FindPluginManager.mFindPlugin.mListFindLayout,
 			    "layout", getPackageName());
 		
-		ClpFindsListAdapter adapter = new ClpFindsListAdapter(this,
+		ShFindsListAdapter adapter = new ShFindsListAdapter(this,
 				resId, list);
 
 		return adapter;
@@ -61,13 +76,18 @@ public class ShListFindsActivity extends ListFindsActivity {
 	 * take care of displaying the extra fields in OutsideInFind.
 	 * 
 	 */
-	private class ClpFindsListAdapter extends FindsListAdapter{
+	private class ShFindsListAdapter extends FindsListAdapter{
 
-		public ClpFindsListAdapter(Context context, int textViewResourceId,
+		public ShFindsListAdapter(Context context, int textViewResourceId,
 				List list) {
 			super(context, textViewResourceId, list);
 		}
 
+		/**
+		 * Displays the ShFind data in a list_row view.  This is the only
+		 * method that differs from one derived Find to another. So perhaps
+		 * this can be abstracted further. 
+		 */
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View v = convertView;
@@ -94,7 +114,7 @@ public class ShListFindsActivity extends ListFindsActivity {
 				tv = (TextView) v.findViewById(R.id.status);
 				tv.setText(find.getStatusAsString());	
 				
-				// This is the only part of this view that differs from Basic
+				// This is the only part of this view that differs from Basic.
 				tv = (TextView) v.findViewById(R.id.stop_type);
 				if (find.getStopType() == ShFind.PICKUP) 
 					tv.setText("Pickup");
