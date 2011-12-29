6d5eb22fe55efef2a8e21d2e81cead28b7c7cf46
Ralph Morelli <ram8647@gmail.com>
2011-12-29 16:27:28 -0500

    Removes unused files and updates documentation in POSIT's database folder.

--- src/org/hfoss/posit/android/experimental/api/database/FindContentProvider.java
+++ src/org/hfoss/posit/android/experimental/api/database/FindContentProvider.java
@@ -1,3 +1,25 @@
+/*
+ * File: FindContentProvider.java
+ * 
+ * Copyright (C) 2011 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of POSIT, Portable Open Source Information Tool.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
+ * 
+ */
+
 package org.hfoss.posit.android.experimental.api.database;
 
 import android.content.ContentProvider;
@@ -5,6 +27,11 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 
+/**
+ * Not sure how this is used but it seems to be required
+ * during Syncing.  Syncing won't work without it.
+ *
+ */
 public class FindContentProvider extends ContentProvider {
 
 	@Override
