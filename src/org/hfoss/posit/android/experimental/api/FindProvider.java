4bc3b42afe760d39ed1a81405de34d8b9a048e34
Ralph Morelli <ram8647@gmail.com>
2011-12-29 15:46:49 -0500

    Updates the documentation in the api folder.

--- src/org/hfoss/posit/android/experimental/api/FindProvider.java
+++ src/org/hfoss/posit/android/experimental/api/FindProvider.java
@@ -1,3 +1,25 @@
+/*
+ * File: FindProvider.java
+ * 
+ * Copyright (C) 2009 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of POSIT, Portable Open Source Information Tool.
+ *
+ * POSIT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
+ * 
+ */
+
 package org.hfoss.posit.android.experimental.api;
 
 import org.hfoss.posit.android.experimental.plugin.FindPluginManager;
@@ -5,7 +27,10 @@ import org.hfoss.posit.android.experimental.plugin.FindPluginManager;
 import android.content.Context;
 
 /**
-* Convenience class to quickly get right find object without having to type  
+* Convenience class to quickly get right find object 
+* without having to type.
+* 
+* TODO: Make this functional or delete it.
 */
 public class FindProvider{
 	private FindProvider(){}
