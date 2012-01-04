2f3a74b1ca9eddca728c6e70bde3d0a8f6b0d8db
Ralph Morelli <ram8647@gmail.com>
2012-01-04 03:35:42 -0500

    FIxes database not open exception in ToDoReminderService.

--- src/org/hfoss/posit/android/functionplugin/reminder/ToDoReminderService.java
+++ src/org/hfoss/posit/android/functionplugin/reminder/ToDoReminderService.java
@@ -22,7 +22,6 @@
 
 package org.hfoss.posit.android.functionplugin.reminder;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -57,53 +56,53 @@ import android.util.Log;
  * 
  **/
 public class ToDoReminderService extends Service  implements LocationListener  {
-	
+
 	private static final String TAG = "LocationService";
 	// Time interval used to receive location
 	// updates from the Location Manager
 	private static final int ONE_MINUTE = 60 * 1000;
-	
+
 	// Project currently associated 
 	private int projectID;
 	// A list of all finds with projectID that have reminders set
 	private ArrayList<Find> reminderFinds = null;
 	// A list of all finds whose reminders have already been sent
 	private ArrayList<Integer> reminderIDs = null;
-	
+
 	// Location variables
 	private LocationManager mLocationManager = null;
 	private Location mCurrentLocation = null;
-	
+
 	private DbManager dbManager = null;
-	
+
 	private NotificationManager mNotificationManager = null;
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+
 	@Override
 	public void onCreate() {
 		Log.i(TAG, "onCreate()");
-		
+
 		// Set the current project
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		projectID = prefs.getInt(getString(R.string.projectPref), 0);
-		
+
 		// Initialize variables
-		
+
 		reminderFinds = new ArrayList<Find>();
 		reminderIDs = new ArrayList<Integer>();
 
 		// Initialize Managers
 		mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		
+
 		String ns = Context.NOTIFICATION_SERVICE;
 		mNotificationManager = (NotificationManager) getSystemService(ns);
-				
+
 	}
-	
+
 	@Override
 	/*
 	 * This is called right after onCreate and is also called
@@ -111,15 +110,15 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 	 */
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.i(TAG, "onStartCommand()");
-		
+
 		// Get the preferences from the Preference Manager
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		boolean allowReminder = prefs.getBoolean("allowReminderKey", true);
 		boolean allowGeoTag = prefs.getBoolean("geotagKey", true);
 		Log.i(TAG, "rmdrPref=" + allowReminder + " geoPref=" + allowGeoTag);
-		
+
 		if (allowReminder && allowGeoTag) {
-			
+
 			// The user allows reminders to be set
 			// Request Location updates every minute
 			mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, ONE_MINUTE, 0, this);
@@ -128,7 +127,7 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 			// Get last known location
 			Location netLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 			Location gpsLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-			
+
 			// Set current location, with priority given to GPS location
 			if (gpsLocation != null) {
 				mCurrentLocation = gpsLocation;
@@ -140,16 +139,16 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 			stopService();
 			return START_STICKY;
 		}
-		
+
 		// The user allows reminders to be set
 		// Request Location updates every minute
 		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, ONE_MINUTE, 0, this);
 		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, ONE_MINUTE, 0, this);
-		
+
 		// Get last known location
 		Location netLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		Location gpsLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		
+
 		// Set current location, with priority given to GPS location
 		if (gpsLocation != null) {
 			mCurrentLocation = gpsLocation;
@@ -157,51 +156,34 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 			mCurrentLocation = netLocation;
 			Log.i(TAG, mCurrentLocation.toString());
 		}
-		
+
 		dbManager = DbHelper.getDbManager(this);
 
 		reminderFinds = refreshReminderList(reminderFinds);
-		
-//		reminderFinds.clear();
-//		
-//		// Find all finds that have reminders attached
-//		for (Find find : dbManager.getFindsByProjectId(projectID)) {
-//			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//			String time = dateFormat.format(find.getTime());
-//			if (time.substring(11).equals("00:00:00")){
-//				reminderFinds.add(find);
-//			}
-//		}
-		
-//		// If there is no finds with reminders, stop the service
-//		if (reminderFinds.size() <= 0) {
-//			Log.i(TAG,"Stopping service, no reminders");
-//			stopService();
-//			return START_STICKY;
-//		}
+
 
 		return START_STICKY;
 	}
-	
+
 	private ArrayList<Find> refreshReminderList(ArrayList<Find> reminderFinds) {
 		reminderFinds.clear();
 		reminderIDs.clear();
 
+		dbManager = DbHelper.getDbManager(this);
+
 		// Find all finds that have reminders attached
 		for (Find find : dbManager.getFindsByProjectId(projectID)) {
 			int is_adhoc = find.getIs_adhoc();
-//			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//			String time = dateFormat.format(find.getTime());
-//			if (time.substring(11).equals("00:00:00")){
+
 			if (is_adhoc == SetReminder.REMINDER_SET) {
 				reminderFinds.add(find);
 				reminderIDs.add(find.getId());
 			}
 		}
-		
+
 		return reminderFinds;
 	}
-	
+
 	/**
 	 * Helper method to stop the service and clean up.
 	 */
@@ -211,14 +193,14 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 			mLocationManager.removeUpdates(this);
 			Log.i(TAG, "Location Updates Cancelled!");
 		}
-		
+
 		// Release DbManager resources
 		if (dbManager != null) {
 			DbHelper.releaseDbManager();
 			dbManager = null;
 			Log.i(TAG, "Releasing Db manager!");
 		}
-		
+
 		//  Cancel all notifications
 		if (mNotificationManager != null) {
 			mNotificationManager.cancelAll();
@@ -227,12 +209,12 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 		Log.i(TAG, "Stopping self");
 		this.stopSelf();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		Log.i(TAG, "onDestroy()");
 	}
-	
+
 	/*
 	 * This is called every time the location is changed,
 	 * or at the end of the indicated time interval (one
@@ -241,45 +223,45 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 	 * @param location  the new location received from Location Manager
 	 */
 	public void onLocationChanged(Location location) {
-		
+
 		Log.i(TAG, "Location might have changed.");
-		
+
 		// If the new location is better, replace the old one with the new one
 		if (isBetterLocation(location, mCurrentLocation)) {
 			mCurrentLocation = location;
 			Log.i(TAG, "Got a new location: " + mCurrentLocation.getLatitude() + "," + mCurrentLocation.getLongitude());
 		}
-		
+
 		// Update the reminderList
 		reminderFinds = refreshReminderList(reminderFinds);
-		
+
 		// Get the longitude and latitude
 		double currLong = mCurrentLocation.getLongitude();
 		double currLat = mCurrentLocation.getLatitude();
-		
+
 		// Go through all the finds with reminders
 		// and check if the user is close to any of
 		// the set locations
 		for (Find find : reminderFinds) {
-			
+
 			// The reminder time should be for today
 			Date findTime = find.getTime();
 			Date currTime = new Date();
 			if ((findTime.getYear() == currTime.getYear())
 					&& (findTime.getMonth() == currTime.getMonth())
 					&& (findTime.getDate() == currTime.getDate())) {
-				
+
 				// The current location should be within a radius of
 				// around 500 meters from reminder's location
 				double findLong = find.getLongitude();
 				double findLat = find.getLatitude();
 				double longDiff = Math.abs(currLong - findLong);
 				double latDiff = Math.abs(currLat - findLat);
-				
-//				if (longDiff <= 0.003 && latDiff <= 0.003) {
+
+				//				if (longDiff <= 0.003 && latDiff <= 0.003) {
 				if (longDiff <= 0.03 && latDiff <= 0.03) {
 					Log.i(TAG, "Trigger a notification");
-					
+
 					// Send the find's notification if it has not yet
 					// been sent, this is to ensure that the notification
 					// will only be sent once
@@ -287,32 +269,29 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 					if (reminderIDs.contains(find.getId())) {
 						Log.i(TAG, "Reminder IDs contains find");
 
-//						// Add find's ID to the list
-//						reminderIDs.add(find.getId());
-						
 						// Set notification icon, name, and time
 						int icon = android.R.drawable.stat_sys_warning;
 						CharSequence tickerText = "Reminder: " + find.getName();
 						long when = System.currentTimeMillis();
 						Notification notification = new Notification(icon, tickerText, when);
 						notification.defaults |= Notification.DEFAULT_SOUND;
-						
+
 						// Set intent and bundle with find's ID
 						Intent intent = new Intent(this, NotifyReminder.class);
 						Bundle bundle = new Bundle();
 						bundle.putInt(Find.ORM_ID, find.getId());
 						intent.putExtras(bundle);
-						
+
 						// Update the Find's status
 						find.setIs_adhoc(SetReminder.REMINDER_NOTIFIED);
 						dbManager.update(find);
-						
+
 						// Set pending intent and send the notification
 						PendingIntent contentIntent = PendingIntent.getActivity(this, find.getId(), intent, 0);
 						notification.setLatestEventInfo(this, "Reminder: " + find.getName(), find.getDescription(),
 								contentIntent);
 						mNotificationManager.notify(find.getId(), notification);
-						
+
 					} else {
 						Log.i(TAG, "Reminder IDs does NOT contain find");
 
@@ -329,55 +308,55 @@ public class ToDoReminderService extends Service  implements LocationListener  {
 	 * @param currentBestLocation  The current Location fix, to which you want to compare the new one
 	 */
 	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
-	    if (currentBestLocation == null) {
-	        // A new location is always better than no location
-	        return true;
-	    }
+		if (currentBestLocation == null) {
+			// A new location is always better than no location
+			return true;
+		}
 
-	    // Check whether the new location fix is newer or older
-	    long timeDelta = location.getTime() - currentBestLocation.getTime();
-	    boolean isSignificantlyNewer = timeDelta > ONE_MINUTE * 2;
-	    boolean isSignificantlyOlder = timeDelta < - ONE_MINUTE * 2;
-	    boolean isNewer = timeDelta > 0;
+		// Check whether the new location fix is newer or older
+		long timeDelta = location.getTime() - currentBestLocation.getTime();
+		boolean isSignificantlyNewer = timeDelta > ONE_MINUTE * 2;
+		boolean isSignificantlyOlder = timeDelta < - ONE_MINUTE * 2;
+		boolean isNewer = timeDelta > 0;
 
-	    // If it's been more than two minutes since the current location, use the new location
-	    // because the user has likely moved
-	    if (isSignificantlyNewer) {
-	        return true;
-	    // If the new location is more than two minutes older, it must be worse
-	    } else if (isSignificantlyOlder) {
-	        return false;
-	    }
+		// If it's been more than two minutes since the current location, use the new location
+		// because the user has likely moved
+		if (isSignificantlyNewer) {
+			return true;
+			// If the new location is more than two minutes older, it must be worse
+		} else if (isSignificantlyOlder) {
+			return false;
+		}
 
-	    // Check whether the new location fix is more or less accurate
-	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
-	    boolean isLessAccurate = accuracyDelta > 0;
-	    boolean isMoreAccurate = accuracyDelta < 0;
-	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+		// Check whether the new location fix is more or less accurate
+		int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+		boolean isLessAccurate = accuracyDelta > 0;
+		boolean isMoreAccurate = accuracyDelta < 0;
+		boolean isSignificantlyLessAccurate = accuracyDelta > 200;
 
-	    // Check if the old and new location are from the same provider
-	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
-	            currentBestLocation.getProvider());
+		// Check if the old and new location are from the same provider
+		boolean isFromSameProvider = isSameProvider(location.getProvider(),
+				currentBestLocation.getProvider());
 
-	    // Determine location quality using a combination of timeliness and accuracy
-	    if (isMoreAccurate) {
-	        return true;
-	    } else if (isNewer && !isLessAccurate) {
-	        return true;
-	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-	        return true;
-	    }
-	    return false;
+		// Determine location quality using a combination of timeliness and accuracy
+		if (isMoreAccurate) {
+			return true;
+		} else if (isNewer && !isLessAccurate) {
+			return true;
+		} else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+			return true;
+		}
+		return false;
 	}
 
 	/* Checks whether two providers are the same */
 	private boolean isSameProvider(String provider1, String provider2) {
-	    if (provider1 == null) {
-	      return provider2 == null;
-	    }
-	    return provider1.equals(provider2);
+		if (provider1 == null) {
+			return provider2 == null;
+		}
+		return provider1.equals(provider2);
 	}
-	
+
 	public void onProviderDisabled(String provider) {}
 
 	public void onProviderEnabled(String provider) {}
