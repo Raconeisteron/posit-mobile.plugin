2d04cce50c01d6105c1f66d1519346cfc4039dd9
Ralph Morelli <ram8647@gmail.com>
2012-03-23 18:17:23 -0400

    Revised the authentication prompt to tell new users that they have to create an account on the server before they can use POSIT.  Also made minor changes to the About page.

--- src/org/hfoss/posit/android/AboutActivity.java
+++ src/org/hfoss/posit/android/AboutActivity.java
@@ -25,7 +25,9 @@ import org.hfoss.posit.android.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.TextView;
 
 
 /**
@@ -42,6 +44,12 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
 		setContentView(R.layout.about_copyright);
+        // Prompt for new users
+		String server = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.serverPref), "");
+        if (server == null)
+        	server = getString(R.string.defaultServer);
+        TextView serverTView = (TextView) findViewById(R.id.currentServer);
+        serverTView.setText("Synced with:\n" + server);
 	}
 
 	@Override
