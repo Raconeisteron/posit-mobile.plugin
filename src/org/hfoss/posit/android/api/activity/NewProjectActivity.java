4ae9cb8e6a5a6ede2bece7edeca1556d2158c1c1
Rob McDiarmid <RobIanMcd@gmail.com>
2012-04-05 09:27:31 -0400

    Rob's updates for Issue #340.  I couldn't find the question_mark.png resource so I downloaded one but it is the wrong size.  The question_mark.png resource file needs to be replaced.

--- src/org/hfoss/posit/android/api/activity/NewProjectActivity.java
+++ src/org/hfoss/posit/android/api/activity/NewProjectActivity.java
@@ -23,16 +23,23 @@
 package org.hfoss.posit.android.api.activity;
 
 import org.hfoss.posit.android.R;
+import org.hfoss.posit.android.api.plugin.FindPluginManager;
 import org.hfoss.posit.android.sync.Communicator;
+import org.w3c.dom.Node;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,11 +51,13 @@ import android.widget.Toast;
 public class NewProjectActivity extends Activity implements OnClickListener{
 
 	private Button mCreateProject;
+	private Spinner mTypeOfFind;
 	private static final String TAG = "NewProjectActivity";
+	private Context mContext;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		mContext = this;
 		if (!Communicator.isServerReachable(this)) {
 		    Log.i(TAG, "Can't add new project. The server not reachable");
 		    Toast.makeText(this, "Can't add new project. The server not reachable.", Toast.LENGTH_LONG).show();
@@ -57,8 +66,33 @@ public class NewProjectActivity extends Activity implements OnClickListener{
 		}
 
 		setContentView(R.layout.new_project);
+		
 		mCreateProject = (Button) this.findViewById(R.id.createProject);
 		mCreateProject.setOnClickListener(this);
+		
+		mTypeOfFind = (Spinner) this.findViewById(R.id.projectFindType);
+		ArrayAdapter<String> typeOfFindArrayAdapter = new ArrayAdapter<String>(this,
+		        	android.R.layout.simple_spinner_item,
+		            FindPluginManager.mFindPluginNames);
+		typeOfFindArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mTypeOfFind.setAdapter(typeOfFindArrayAdapter);
+		mTypeOfFind.setSelection(FindPluginManager.mFindPluginNames.indexOf(FindPluginManager.mDefaultFindPluginName));
+		
+		ImageButton pluginDescriptionBtn = (ImageButton)this.findViewById(R.id.find_info_btn);
+		pluginDescriptionBtn.setOnClickListener( new OnClickListener() {
+				
+			public void onClick(View v) {
+     		String projectFindType = mTypeOfFind.getSelectedItem().toString();
+     		Node pluginInfo = FindPluginManager.getInstance().GetStaticPluginInfoFromPluginName(projectFindType);
+     		String description = pluginInfo.getAttributes().getNamedItem("description").getTextContent();
+     		
+				AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+				builder.setTitle(projectFindType)
+					   .setMessage(description)
+					   .setPositiveButton("OK", null)
+					   .show();
+			}
+		});
 	}
 
 	public void onClick(View v) {
@@ -66,6 +100,8 @@ public class NewProjectActivity extends Activity implements OnClickListener{
 		case R.id.createProject:
 			String projectName = (((TextView) findViewById(R.id.projectName)).getText()).toString();
 			String projectDescription = (((TextView) findViewById(R.id.projectDescription)).getText()).toString(); 
+			//TODO: Send the project find type to the server.
+			String projectFindType = mTypeOfFind.getSelectedItem().toString();
 			if(projectName.equals("")){
 				Toast.makeText(this, "Please enter a name for your project", Toast.LENGTH_LONG).show();
 				break;
