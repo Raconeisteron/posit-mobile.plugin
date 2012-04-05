5ce86aa6b46ad6a48a258b87fbaa3386c3574728
Andrew Matsusaka <and.matsusaka@gmail.com>
2012-04-05 09:44:25 -0400

    This patch helps another area of development continue progress. The next patch will include the completed SyncSms and additional commenting.
    Changes include:
      -Added methods for getting list of available projects from Server
      -Added methods for setting current project
      -Cleaned up imports in SyncSms
    
    In progress:
      -Adding java doc for all new functions added
      -Adding boilerplate copyright info for new files added
      -Testing SyncSms

--- src/org/hfoss/posit/android/sync/SyncServer.java
+++ src/org/hfoss/posit/android/sync/SyncServer.java
@@ -1,3 +1,24 @@
+/*
+ * File: SyncServer.java
+ * 
+ * Copyright (C) 2011 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of POSIT, Portable Open Source Information Tool. 
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
+ * 
+ */
 package org.hfoss.posit.android.sync;
 
 import java.io.FileNotFoundException;
@@ -7,6 +28,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -25,11 +47,24 @@ import org.json.JSONObject;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
+/**
+ * Synchronization class used to for sync processes with the server. The
+ * core functionality of this class is to receive finds and send finds.
+ * 
+ * The server uses a different format for Finds in string format, thus it
+ * is implemented differently here. In addition, this class can be used to
+ * change the current project and retrieve the list of projects from
+ * the server.
+ * 
+ * @author Andrew Matsusaka
+ *
+ */
 public class SyncServer extends SyncMedium{
 	public final int CONNECTION_TIMEOUT = 3000;
 	public final int SOCKET_TIMEOUT = 5000;
@@ -52,27 +87,125 @@ public class SyncServer extends SyncMedium{
 	private String m_server;
 	private String m_imei;
 	
+	/**
+	 * Constructor that accepts the Context and initializes all the necessary
+	 * settings
+	 * @param context - Context to be used for all sync purposes
+	 */
 	public SyncServer(Context context){
 		m_context = context;
 		initSettings();
 	}
 	
+	/**
+	 * Initializes the settings needed to sync with the server
+	 */
 	private void initSettings(){
 		initPreferences();
 		initTelephony();
+		initAuthKey();
 	}
 	
+	/**
+	 * Initializes the server string and project ID
+	 */
 	private void initPreferences(){
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(m_context);
 		m_server = prefs.getString(SERVER_PREF, "");
 		m_projectId = prefs.getInt(PROJECT_PREF, 0);
 	}
 	
+	/**
+	 * Initializes the imei id
+	 */
 	private void initTelephony(){
 		TelephonyManager telephonyManager = (TelephonyManager) m_context.getSystemService(Context.TELEPHONY_SERVICE);
 		m_imei = telephonyManager.getDeviceId();
 	}
 	
+	/**
+	 * Initializes the authorization key
+	 */
+	private void initAuthKey(){
+		m_authKey = Communicator.getAuthKey( m_context );
+	}
+	
+	/**
+	 * Sends a query to the server for a list of the current users projects.
+	 * Returns the result in the form of a List of HashMaps
+	 * @return List of HashMaps containing the project data
+	 */
+	public List<HashMap<String,Object>> getProjects(){
+		ArrayList<HashMap<String, Object>> list = null;
+
+		String url = m_server + "/api/listMyProjects?authKey=" + m_authKey;
+
+		String responseString = Communicator.doHTTPGET(url);
+		Log.i(TAG, responseString);
+
+		if (!responseString.contains("Error")) {
+			try {
+				list = (ArrayList<HashMap<String, Object>>) (new ResponseParser(responseString).parseList());
+			} catch (JSONException e) {
+				Log.i(TAG, "getProjects JSON exception " + e.getMessage());
+				list = null;
+			}
+		}
+		
+		return list;
+	}
+	
+	/**
+	 * Extracts the list of project names from the List of HashMap project data
+	 * @param projects - List of HashMap project data to extract data from
+	 * @return List of strings of project names
+	 */
+	public List<String> getProjectStrings(List<HashMap<String,Object>> projects) {
+		List<HashMap<String,Object>> projectList = new ArrayList<HashMap<String,Object>>();
+		Iterator<HashMap<String, Object>> it 	 = projectList.iterator();
+		ArrayList<String> projList 				 = new ArrayList<String>();
+		
+		while( it.hasNext() ) {
+			HashMap<String,Object> next = it.next();
+			projList.add((String)(next.get("name")));
+		}
+		return projList;
+	}
+	
+	/**
+	 * Accepts a HashMap of project data to set the new current project to
+	 * @param newProject - HashMap of project data
+	 * @return whether or not the switching of projects was successful
+	 */
+	public boolean setProject( HashMap<String,Object> newProject ){
+		String projectId 		= (String) newProject.get("id");
+		String projectName 		= (String) newProject.get("name");
+		String projectPref  	= m_context.getString(R.string.projectPref);
+		String projectNamePref 	= m_context.getString(R.string.projectNamePref);
+		int id  				= Integer.parseInt(projectId);
+		boolean success 		= true;
+
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(m_context);
+		int currentProjectId = sp.getInt(projectPref,0);
+		
+		if (id == currentProjectId){
+			success = false;
+		}
+		else {
+			Editor editor = sp.edit();
+
+			editor.putInt(projectPref, id);
+			editor.putString(projectNamePref, projectName);
+			editor.commit();
+		}
+		
+		return success;
+	}
+	
+	/**
+	 * Gives back a list of Find guids that are not synced with the server
+	 * @return List of string guids that need to be synced
+	 */
 	public List<String> getFindsNeedingSync(){
 		String serverFindsIds = getServerFindsNeedingSync();
 		List<String> finds = new ArrayList<String>();
@@ -85,6 +218,10 @@ public class SyncServer extends SyncMedium{
 		return finds;
 	}
 	
+	/**
+	 * Gives back a String of Find guids that are not synced with the server
+	 * @return String that is a comma separated list of Find guids
+	 */
 	private String getServerFindsNeedingSync() {
 		String response = "";
 		
@@ -101,6 +238,11 @@ public class SyncServer extends SyncMedium{
 		return response;
 	}
 	
+	/**
+	 * Queries the server for the find data corresponding to the passed in guid
+	 * @param guid - Guid of the find to get data for
+	 * @return Find data in the form of a string
+	 */
 	public String retrieveRawFind( String guid ){
 		String url = m_server + "/api/getFind?guid=" + guid + "&authKey=" + m_authKey;
 		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
@@ -114,6 +256,11 @@ public class SyncServer extends SyncMedium{
 		return responseString;
 	}
 
+	/**
+	 * Sends a find to the server
+	 * @param find - find to be sent
+	 * @return whether or not the sending was successful
+	 */
 	public boolean sendFind( Find find ){
 		boolean success = false;
 		String url = createActionBasedUrl( find );
@@ -139,6 +286,11 @@ public class SyncServer extends SyncMedium{
 		return success;
 	}
 	
+	/**
+	 * Creates a url string based on the find's action (Create or Update)
+	 * @param find - Find used to determine action url
+	 * @return url containing action
+	 */
 	private String createActionBasedUrl( Find find ){
 		String url = "";
 		String action = find.getAction();
@@ -156,6 +308,11 @@ public class SyncServer extends SyncMedium{
 		return url;
 	}
 	
+	/**
+	 * Converts the data from a Find object into NameValuePairs
+	 * @param find - find to have data extracted from
+	 * @return List of NameValuePairs containing Find data
+	 */
 	private List<NameValuePair> getNameValuePairs(Find find) {
 		List<NameValuePair> pairs = null;
 		if (find.getClass().getName().equals(Find.class.getName())) {
@@ -221,6 +378,13 @@ public class SyncServer extends SyncMedium{
 		return nvp;
 	}
 	
+	/**
+	 * Transmits a Find to the server using the passed in find, url and NameValuePairs
+	 * @param find - find to be transmitted
+	 * @param url - url to be used to transmit find
+	 * @param pairs - NameValuePairs containing Find data
+	 * @return whether or not the transmission was successful
+	 */
 	private boolean transmitFind( Find find, String url, List<NameValuePair> pairs ){
 		boolean success = true;
 		try {
@@ -238,6 +402,10 @@ public class SyncServer extends SyncMedium{
 		return success;
 	}
 	
+	/**
+	 * Transmits an image associated to the passed in Find
+	 * @param find - Find containing the image to be transmitted
+	 */
 	private void transmitImage( Find find ){
 		if(Camera.isPhotoSynced(find, m_context) == false){
 			HashMap<String, String> sendMap = createImageMap( find );
@@ -245,6 +413,11 @@ public class SyncServer extends SyncMedium{
 	 	}
 	}
 	
+	/**
+	 * Creates a mapping of the find's image data
+	 * @param find - Find containing image
+	 * @return HashMap of image data
+	 */
 	private HashMap<String, String> createImageMap( Find find ){
 		HashMap<String, String> sendMap = new HashMap<String, String>();
 		
@@ -263,6 +436,10 @@ public class SyncServer extends SyncMedium{
 		return sendMap;
 	}
 	
+	/**
+	 * Records the sync on the server as well as the current device
+	 * @return whether or not the recordings were successful
+	 */
 	public boolean postSendTasks(){
 		boolean success = true;
 		
@@ -272,6 +449,10 @@ public class SyncServer extends SyncMedium{
 		return success;
 	}
 	
+	/**
+	 * Records the sync on the server
+	 * @return whether or not the recording was successful
+	 */
 	private boolean recordSyncOnServer() {
 		String url = m_server + "/api/recordSync?authKey=" + m_authKey + "&imei=" + m_imei + "&projectId=" + m_projectId;
 		Log.i(TAG, "recordSync URL=" + url);
@@ -288,12 +469,21 @@ public class SyncServer extends SyncMedium{
 		return true;
 	}
 	
+	/**
+	 * Records the sync on the current device
+	 * @return whether or not the recording was successful
+	 */
 	private boolean recordSyncOnDevice(){
 		int success = 0;
 		success = DbHelper.getDbManager(m_context).recordSync(new SyncHistory("idkwhatthisissupposedtobe"));
 		return success != 0;
 	}
 	
+	/**
+	 * Converts the passed in raw find data into a Find object
+	 * @param rawFind - Find data in string format
+	 * @return Find object filled with data
+	 */
 	public Find convertRawToFind( String rawFind ){
 		Find newFind = new Find();
 		ContentValues cv = getCvFromRaw( rawFind );
@@ -304,6 +494,11 @@ public class SyncServer extends SyncMedium{
 		return newFind;
 	}
 	
+	/**
+	 * Extracts the Find data from the string in the form of ContentValues
+	 * @param rawFind - find data to be extracted
+	 * @return ContentValues containing find data
+	 */
 	private ContentValues getCvFromRaw( String rawFind ){
 		ContentValues cv = new ContentValues();
 
@@ -327,6 +522,11 @@ public class SyncServer extends SyncMedium{
 		return null;
 	}
 	
+	/**
+	 * Fills ContentValues with the basic data of a find using a JSONObject
+	 * @param cv - ContentValues to be filled
+	 * @param find - JSONObject containing basic find data
+	 */
 	private void fillCvWithBasicData( ContentValues cv, JSONObject find ){
 		try{
 			cv.put(Find.GUID, 			find.getString(Find.GUID)		);
@@ -344,6 +544,11 @@ public class SyncServer extends SyncMedium{
 		}
 	}
 	
+	/**
+	 * Fills ContentValues with the extended data of a find using a JSONObject
+	 * @param cv - ContentValues to be filled
+	 * @param find - JSONObject containing extended find data
+	 */
 	private void fillCvWithExtendedData( ContentValues cv, JSONObject jobj ){
 		try{
 			if (jobj.has(Find.EXTENSION)) {
@@ -384,6 +589,11 @@ public class SyncServer extends SyncMedium{
 		}
 	}
 	
+	/**
+	 * Uses the raw find data to extract the image ids, retrieve the images
+	 * from the server and store them on the current device
+	 * @param rawFind - raw find data containing image ids
+	 */
 	private void retrieveImage( String rawFind ){
 		try{
 			JSONObject jobj = new JSONObject(rawFind);
@@ -435,6 +645,11 @@ public class SyncServer extends SyncMedium{
 		return imgId;
 	}
 	
+	/**
+	 * Cleans the image data string by trimming brackets and white space
+	 * @param data - image data to be cleaned
+	 * @return String containing the cleaned data
+	 */
 	private String cleanImageData(String data){
 		String cleaned = data.trim();
 		cleaned = cleaned.substring(1, cleaned.length()-1); //removing brackets
@@ -459,14 +674,21 @@ public class SyncServer extends SyncMedium{
 		return saveImageData( imageResponseString );
 	 }
 	
+	/**
+	 * Creates a HashMap containing the imei
+	 * @return HashMap containing the imei
+	 */
 	private HashMap<String, String> createSendMap(){
-		//TODO: If Communicator.getIMEI is just returning m_imei, then
-		//		use this code in createImageMap
 		HashMap<String, String> sendMap = new HashMap<String, String>();
 		sendMap.put(COLUMN_IMEI, Communicator.getIMEI(m_context));
 		return sendMap;
 	}
 	
+	/**
+	 * Uses the image response string from server to extract an image and save it
+	 * @param imageResponseString - string containing image data
+	 * @return whether or not extract and saving the image was successful
+	 */
 	private boolean saveImageData( String imageResponseString ){
 		boolean success = true;
 		
